5b9c45013529089df69261f2b255447147e427d9
NAKITARE00 <paulnakitare08@gmail.com>
2025-03-19 22:06:12 +0300

    ADDRESSES AND PRODUCTS

--- artisalink/config/db.js
+++ artisalink/config/db.js
@@ -1,31 +1,34 @@
 import mongoose from "mongoose";
-import { NEXT_CACHE_TAGS_HEADER } from "next/dist/lib/constants";
 
-let cached =  global.mongoose;
+let cached = global.mongoose;
 
 if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+    cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function connectDB() {
     if (cached.conn) {
+        console.log("Using cached MongoDB connection");
         return cached.conn;
     }
 
     if (!cached.promise) {
-        const opts = {
-            bufferCommands: false,
-        }
+        console.log("Creating new MongoDB connection...");
+        const opts = { bufferCommands: false };
 
-        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/artisalink`, opts).then((mongoose) => {
-            console.log("Connected to MongoDB");
-            return mongoose;
-        })
-
-        cached.conn = await cached.promise;
-        return cached.conn;
+        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/artisalink`, opts)
+            .then((mongoose) => {
+                console.log("Connected to MongoDB");
+                return mongoose;
+            })
+            .catch((err) => {
+                console.error("MongoDB Connection Error:", err);
+                throw err;
+            });
     }
 
+    cached.conn = await cached.promise;
+    return cached.conn;
 }
 
 export default connectDB;
